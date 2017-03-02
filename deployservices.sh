ed7b33d9fb2c16258d88fcfa4b92b50299d4a634
Miguel <mbouchan@resuelve.mx>
2017-03-02 16:04:41 -0600

    add nacho3

--- deployservices.sh
+++ deployservices.sh
@@ -1,8 +1,17 @@
 cd serverless-h1mpex
-cd cactividadprofesional
-zip -r cactividadprofesional.zip .
-aws lambda update-function-code --function-name latasa-cactividadprofesional --zip-file fileb://cactividadprofesional.zip --publish
-lambda_publish="$(aws lambda publish-version --function-name 'arn:aws:lambda:us-east-1:944195423972:function:latasa-cactividadprofesional' --description '1' --region 'us-east-1')"
-export PYTHONIOENCODING=utf8
-lambda_version="$(echo $lambda_publish | /usr/bin/python -c 'import sys, json; print json.load(sys.stdin)["Version"]')"
-aws lambda update-alias --function-name latasa-cactividadprofesional --name "dev" --function-version $lambda_version
+
+array=( cactividadprofesional )
+for i in "${array[@]}"
+do
+    $i
+    cd $i
+    zip -r $i.zip .
+    aws lambda update-function-code --function-name latasa-$i --zip-file fileb://$i.zip --publish
+    lambda_publish="$(aws lambda publish-version --function-name 'arn:aws:lambda:us-east-1:944195423972:function:latasa-$i' --description '1' --region 'us-east-1')"
+    echo $lambda_publish
+    export PYTHONIOENCODING=utf8
+    lambda_version="$(echo $lambda_publish | /usr/bin/python -c 'import sys, json; print json.load(sys.stdin)["Version"]')"
+    echo $lambda_version
+    aws lambda update-alias --function-name latasa-cactividadprofesional --name "dev" --function-version $lambda_version
+    cd..
+done
