4a34b503e12c2323d56b9568634ce16c7289b8ba
Miguel <mbouchan@resuelve.mx>
2017-03-02 16:18:23 -0600

    add nacho4

--- deployservices.sh
+++ deployservices.sh
@@ -3,14 +3,17 @@ cd serverless-h1mpex
 array=( cactividadprofesional )
 for i in "${array[@]}"
 do
-    $i
     cd $i
     zip -r $i.zip .
     aws lambda update-function-code --function-name latasa-$i --zip-file fileb://$i.zip --publish
-    lambda_publish="$(aws lambda publish-version --function-name 'arn:aws:lambda:us-east-1:944195423972:function:latasa-cactividadprofesional' --description '1' --region 'us-east-1')"
+    lambdaaws="'arn:aws:lambda:us-east-1:944195423972:function:latasa-"$i"'"
+    echo "valor lambdaaws"
+    echo $lambdaaws
+    lambda_publish="$(aws lambda publish-version --function-name $lambdaaws --description '1' --region 'us-east-1')"
     echo $lambda_publish
     export PYTHONIOENCODING=utf8
     lambda_version="$(echo $lambda_publish | /usr/bin/python -c 'import sys, json; print json.load(sys.stdin)["Version"]')"
     echo $lambda_version
-    aws lambda update-alias --function-name latasa-cactividadprofesional --name "dev" --function-version $lambda_version
+    aws lambda update-alias --function-name latasa-$i --name "dev" --function-version $lambda_version
+    cd -
 done
--- serverless-h1mpex/cactividadprofesional/handler.js
+++ serverless-h1mpex/cactividadprofesional/handler.js
@@ -53,7 +53,7 @@ module.exports.handler = function(event, context, cb) {
         message:"",
         p_data:lista,
         tamaño:lista.length,
-        otro:"nachopolis"
+        otro:"nachopolis333"
       });
     }
   });
