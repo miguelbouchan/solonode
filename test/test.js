0be2caf0c86071235b80f1352ff83a67f331a035
Miguel <mbouchan@resuelve.mx>
2017-03-02 17:09:12 -0600

    add second service2

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var event2 = {};
 var func = [
 //["buscaUserOracle", jsonuser, 'serverless-h1mpex/buscauseroracle/handler.js'],
 ["catalogoActividadProfesional", event2, 'serverless-h1mpex/cactividadprofesional/handler.js'],
-["servicioprueba2", event2, 'serverless-h1mpex/servicioprueba2/handler.js']];
+["serviciopruebas", event2, 'serverless-h1mpex/serviciopruebas/handler.js']];
 
 //describe('test to use in serverless', function () {
 //it('should pass if the answer is 0 in each service', function () {
