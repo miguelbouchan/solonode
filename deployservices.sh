90533491b5dd324530d86640140dfd25e13d66bc
Miguel <mbouchan@resuelve.mx>
2017-03-02 16:06:54 -0600

    add nacho3

--- deployservices.sh
+++ deployservices.sh
@@ -7,7 +7,7 @@ do
     cd $i
     zip -r $i.zip .
     aws lambda update-function-code --function-name latasa-$i --zip-file fileb://$i.zip --publish
-    lambda_publish="$(aws lambda publish-version --function-name 'arn:aws:lambda:us-east-1:944195423972:function:latasa-$i' --description '1' --region 'us-east-1')"
+    lambda_publish="$(aws lambda publish-version --function-name 'arn:aws:lambda:us-east-1:944195423972:function:latasa-cactividadprofesional' --description '1' --region 'us-east-1')"
     echo $lambda_publish
     export PYTHONIOENCODING=utf8
     lambda_version="$(echo $lambda_publish | /usr/bin/python -c 'import sys, json; print json.load(sys.stdin)["Version"]')"
