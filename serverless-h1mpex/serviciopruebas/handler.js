87953d90fa0bc0448fbd4e0bc7a2baeae186f22a
Miguel <mbouchan@resuelve.mx>
2017-03-02 19:17:01 -0600

    new change

--- serverless-h1mpex/serviciopruebas/handler.js
+++ serverless-h1mpex/serviciopruebas/handler.js
@@ -5,7 +5,7 @@ module.exports.handler = function (event, context, cb) {
 	console.log("iniciando funcion de prueba")
 	return cb(null, {
 		err: 0,
-		message: "mensaje de muestra nacho",
+		message: "mensaje de mike",
 		p_data: { datos: "no hay datos" }
 	});
 };
